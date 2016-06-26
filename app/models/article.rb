34c7ee85a9c380864a37ebb535fed5ab119d431d
Jaes W Overley <joverley@cerner.com>
2016-06-26 09:15:25 -0500

    Create articles table, model and add validations

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3, Maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, Maximum: 300 }
+
+end
