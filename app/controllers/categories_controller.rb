970a59460da7e1987853cddf42a7fca06294b154
Jaes W Overley <joverley@cerner.com>
2016-07-13 16:31:45 -0500

    Add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,10 +13,24 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:success] = "Category was created successfully"
+      flash[:success] = "Category created successfully"
       redirect_to categories_path
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
     end
   end
   
